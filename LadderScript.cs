fc66a461aa88288b1f53b633d86a1a1fffeb36ec
LukeNathan <32780865+LukeNathan@users.noreply.github.com>
2020-05-12 16:36:18 -0400

    removed useless update and initialization variable

--- LadderScript.cs
+++ LadderScript.cs
@@ -6,16 +6,6 @@ public class LadderScript : MonoBehaviour {
 
     public float ladderSpeed = 3;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerStay2D(Collider2D other) {
         if(other.tag=="Player" && Input.GetKey(KeyCode.UpArrow)) {
 
