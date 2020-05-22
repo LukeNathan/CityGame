b3c2eafed80c65f4c8545a3c16b592dc82b9d10b
LukeNathan <32780865+LukeNathan@users.noreply.github.com>
2020-05-22 18:12:22 -0400

    updated with game-wide control

--- GameMenu.cs
+++ GameMenu.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMenu : MonoBehaviour {
+public class GameMenu : MonoBehaviour
+{
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("Game");
+    private int nextScene;
+
+    public void loadNext() {
+        SceneManager.LoadScene(nextScene);
+        Debug.Log("LOAD");
     }
 
-    public void QuitGame()
-    {
+    private void Start() { 
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void QuitGame() { 
         Debug.Log("QUIT");
         Application.Quit();
-
     }
 
+    public void Finish() {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("LOAD");
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Player") {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
 }
