17762b731780b358ccb9ecb798d91c1b23724353
LukeNathan <32780865+LukeNathan@users.noreply.github.com>
2020-05-22 18:12:49 -0400

    Update Grounded.cs

--- Grounded.cs
+++ Grounded.cs
@@ -10,15 +10,13 @@ public class Grounded : MonoBehaviour
         Player = gameObject.transform.parent.gameObject;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.tag == "Ground") {
             Player.GetComponent<Move2D>().isGrounded = true;
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
+    private void OnCollisionExit2D(Collision2D collision) {
         if (collision.collider.tag == "Ground") {
             Player.GetComponent<Move2D>().isGrounded = false;
         }
